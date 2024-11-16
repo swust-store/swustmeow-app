40f25739fd3328fb793ab86d2b1ca69e37fc3e13
FoliageOwO <mc.windleaf@foxmail.com>
2024-11-16 22:46:10 +0800

    feat: 实现自动切换亮白和暗黑模式

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:forui/forui.dart';
 import 'package:miaomiaoswust/views/homepage.dart';
 
 void main() => runApp(const Application());
 
-class Application extends StatelessWidget {
+class Application extends StatefulWidget {
   const Application({super.key});
 
   @override
+  State<StatefulWidget> createState() => ApplicationState();
+}
+
+class ApplicationState extends State<Application> with WidgetsBindingObserver {
+  bool isDarkMode = false;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void didChangePlatformBrightness() {
+    super.didChangePlatformBrightness();
+    final isDarkMode = View.of(context).platformDispatcher.platformBrightness ==
+        Brightness.dark;
+    setState(() => this.isDarkMode = isDarkMode);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+    // 反过来，因为暗黑模式下需要白色的状态栏，反之相同
+    final overlayStyle =
+        isDarkMode ? SystemUiOverlayStyle.light : SystemUiOverlayStyle.dark;
+    SystemChrome.setSystemUIOverlayStyle(overlayStyle.copyWith(
+        statusBarColor: Colors.transparent,
+        systemNavigationBarColor: Colors.transparent));
+
+    final theme = isDarkMode ? FThemes.zinc.dark : FThemes.zinc.light;
     final themeData = FThemeData.inherit(
-        colorScheme: FThemes.zinc.light.colorScheme,
-        typography:
-            FThemes.zinc.light.typography.copyWith(defaultFontFamily: '未来圆SC'));
+        colorScheme: theme.colorScheme,
+        typography: theme.typography.copyWith(defaultFontFamily: '未来圆SC'));
+
     return MaterialApp(
       builder: (context, child) => FTheme(data: themeData, child: child!),
       home: const Homepage(),
--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:forui/forui.dart';
+
 // import 'package:miaomiaoswust/components/bottom_navbar.dart';
 
 class Homepage extends StatefulWidget {
@@ -11,10 +12,13 @@ class Homepage extends StatefulWidget {
 
 class HomepageState extends State<Homepage> {
   @override
-  Widget build(BuildContext context) => const FScaffold(
+  Widget build(BuildContext context) => Container(
+      color: context.theme.colorScheme.background,
+      child: const SafeArea(
+          child: FScaffold(
         // footer: BottomNavBar(), // 目前版本不开放底部导航栏
         content: Column(
           children: [],
         ),
-      );
+      )));
 }
