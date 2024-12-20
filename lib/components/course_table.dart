e281bdc523ba6f57a66753bc1fc82f0d0432432e
FoliageOwO <mc.windleaf@foxmail.com>
2024-12-20 14:01:52 +0800

    style(course_table): 优化颜色

--- lib/components/course_table.dart
+++ lib/components/course_table.dart
@@ -153,11 +153,32 @@ class _CourseTableState extends State<CourseTable> {
         entry.weekday == dayIndex + 1 && entry.numberOfDay == rowIndex + 1);
     if (matched.isNotEmpty) {
       final first = matched.first;
+      final active = first.getIsActive();
+      final dark = Values.isDarkMode;
+
+      final bgColor = active
+          ? Color(first.color).withOpacity(dark ? 0.7 : 0.9)
+          : Colors.grey.withOpacity(dark ? 0.2 : 0.4);
+      final primaryColor = Colors.white.withOpacity(dark
+          ? active
+              ? 0.8
+              : 0.4
+          : active
+              ? 1
+              : 0.8);
+      final secondaryColor = Colors.white.withOpacity(dark
+          ? active
+              ? 0.6
+              : 0.2
+          : active
+              ? 0.8
+              : 0.6);
+
       return Container(
           padding: const EdgeInsets.all(4),
           margin: const EdgeInsets.all(1),
           decoration: BoxDecoration(
-            color: first.getIsActive() ? Color(first.color) : Colors.grey[300],
+            color: bgColor,
             borderRadius: const BorderRadius.all(Radius.circular(6)),
           ),
           child: Column(
@@ -165,8 +186,8 @@ class _CourseTableState extends State<CourseTable> {
             children: [
               Text(
                 overflowed(first.courseName, 3 * 3),
-                style: const TextStyle(
-                    color: Colors.white,
+                style: TextStyle(
+                    color: primaryColor,
                     height: 0,
                     fontSize: 12,
                     fontWeight: FontWeight.bold,
@@ -176,7 +197,7 @@ class _CourseTableState extends State<CourseTable> {
               Text(
                 overflowed('@${first.place}', 3 * 3),
                 style: TextStyle(
-                    color: Colors.white.withOpacity(0.7),
+                    color: secondaryColor,
                     height: 0,
                     fontSize: 10,
                     letterSpacing: 0,
