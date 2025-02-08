a84b668b20ebf6fcf4657c1014bfac06b95bb908
FoliageOwO <mc.windleaf@foxmail.com>
2025-02-08 21:45:22 +0800

    feat(soa): 完善课程表页面

--- lib/views/course_table_page.dart
+++ lib/views/course_table_page.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:swustmeow/components/header_selector.dart';
@@ -7,6 +8,7 @@ import 'package:swustmeow/utils/courses.dart';
 import 'package:swustmeow/utils/status.dart';
 
 import '../components/course_table/course_table.dart';
+import '../components/utils/base_header.dart';
 import '../components/utils/base_page.dart';
 import '../services/global_service.dart';
 import '../services/value_service.dart';
@@ -52,79 +54,84 @@ class _CourseTablePageState extends State<CourseTablePage>
     final now = DateTime.now();
     final (_, _, w) =
         GlobalService.termDates.value[term]?.value ?? (now, now, -1);
-    final [ts, te] = [s, e].map((x) => int.parse(x) - 2000).toList();
     final week = w > 0 ? '($w周)' : '';
-    return '$ts-$te-$t$week';
+    return '$s-$e-$t$week';
   }
 
   @override
   Widget build(BuildContext context) {
     final terms = _containers.map((c) => c.term).toList();
-    final titleStyle = TextStyle(fontSize: 14);
+    final titleStyle = TextStyle(fontSize: 14, color: Colors.white);
+
     return Transform.flip(
       flipX: ValueService.isFlipEnabled.value,
       flipY: ValueService.isFlipEnabled.value,
-      child: BasePage.gradient(
-        header: Column(
-          children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 12.0),
-                  child: Text(
-                    '课程表',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 22,
-                      color: Colors.white,
+      child: BasePage.color(
+        headerPad: false,
+        header: BaseHeader(
+          title: HeaderSelector<String>(
+            enabled: !_isLoading,
+            initialValue: _currentContainer.term,
+            onSelect: (value) {
+              final container = _containers.singleWhere((c) => c.term == value);
+              _refresh(() => _currentContainer = container);
+            },
+            count: terms.length,
+            titleBuilder: (context, value) {
+              return Align(
+                alignment: Alignment.centerRight,
+                child: Column(
+                  children: [
+                    Text(
+                      '课程表',
+                      maxLines: 1,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
                     ),
-                  ),
+                    AutoSizeText(
+                      _parseDisplayString(value),
+                      maxLines: 1,
+                      maxFontSize: 12,
+                      minFontSize: 8,
+                      style: TextStyle(color: Colors.white),
+                    )
+                  ],
                 ),
-                HeaderSelector<String>(
-                  enabled: !_isLoading,
-                  initialValue: _currentContainer.term,
-                  onSelect: (value) {
-                    final container =
-                        _containers.singleWhere((c) => c.term == value);
-                    _refresh(() => _currentContainer = container);
-                  },
-                  count: terms.length,
-                  titleBuilder: (context, value) =>
-                      Text(_parseDisplayString(value), style: titleStyle),
-                  tileValueBuilder: (context, index) => terms[index],
-                  tileTextBuilder: (context, index) => terms[index],
-                  fallbackTitle: Text('未知学期', style: titleStyle),
-                ),
-                Spacer(),
-                IconButton(
-                  onPressed: () async {
-                    if (_isLoading) return;
+              );
+            },
+            tileValueBuilder: (context, index) => terms[index],
+            tileTextBuilder: (context, index) => terms[index],
+            fallbackTitle: Text('未知学期', style: titleStyle),
+          ),
+          suffixIcons: [
+            IconButton(
+              onPressed: () async {
+                if (_isLoading) return;
 
-                    _refresh(() => _isLoading = true);
-                    final res =
-                        await GlobalService.soaService!.getCourseTables();
-                    if (res.status != Status.ok) return;
-                    List<CoursesContainer> containers =
-                        (res.value as List<dynamic>).cast();
-                    final current = containers
-                        .where((c) => c.term == _currentContainer.term);
-                    _refresh(() {
-                      _containers = containers;
-                      _currentContainer = current.isNotEmpty
-                          ? current.first
-                          : getCurrentCoursesContainer(
-                              widget.activities, containers);
-                      _isLoading = false;
-                    });
-                  },
-                  icon: FaIcon(
-                    FontAwesomeIcons.rotateRight,
-                    color: Colors.white,
-                    size: 20,
-                  ),
-                ),
-              ],
+                _refresh(() => _isLoading = true);
+                final res = await GlobalService.soaService!.getCourseTables();
+                if (res.status != Status.ok) return;
+                List<CoursesContainer> containers =
+                    (res.value as List<dynamic>).cast();
+                final current =
+                    containers.where((c) => c.term == _currentContainer.term);
+                _refresh(() {
+                  _containers = containers;
+                  _currentContainer = current.isNotEmpty
+                      ? current.first
+                      : getCurrentCoursesContainer(
+                          widget.activities, containers);
+                  _isLoading = false;
+                });
+              },
+              icon: FaIcon(
+                FontAwesomeIcons.rotateRight,
+                color: Colors.white,
+                size: 20,
+              ),
             ),
           ],
         ),
