35ebd12739a5db3d11ad592d8b84c59d80c98436
FoliageOwO <mc.windleaf@foxmail.com>
2025-02-12 01:21:30 +0800

    feat: 添加友盟统计 SDK

--- android/app/src/main/kotlin/store/swust/swustmeow/MainActivity.kt
+++ android/app/src/main/kotlin/store/swust/swustmeow/MainActivity.kt
@@ -1,5 +1,12 @@
 package store.swust.swustmeow
 
+import android.os.Bundle
+import com.umeng.commonsdk.UMConfigure
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        UMConfigure.preInit(this, "REDACTED_UMENG_APP_KEY", "Umeng")
+    }
+}
