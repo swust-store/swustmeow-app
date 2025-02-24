aac93e4a4e85dbfdb79300ca7b00bdaccf877589
mac <mac@d66-183-208-45.bchsia.telus.net>
2025-02-24 13:12:39 +0800

    feat(route): 支持 iOS 原生侧滑返回手势

--- lib/utils/router.dart
+++ lib/utils/router.dart
@@ -1,29 +1,39 @@
+import 'dart:io' show Platform; // 导入 Platform 类
+
 import 'package:animations/animations.dart';
+import 'package:flutter/cupertino.dart'; // 导入 CupertinoPageRoute
 import 'package:flutter/material.dart';
 
 Route _buildRoute(Widget widget, {required bool pushInto}) {
-  return PageRouteBuilder(
-    pageBuilder: (context, animation, secondaryAnimation) => widget,
-    transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      const begin = Offset(1.0, 0.0); // 从右侧滑入
-      const end = Offset(0.0, 0.0); // 最终位置
-      const curve = Curves.easeInOut; // 动画曲线
+  if (Platform.isIOS) {
+    return CupertinoPageRoute(
+      builder: (context) => widget,
+    );
+  } else {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => widget,
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        const begin = Offset(1.0, 0.0);
+        const end = Offset(0.0, 0.0);
+        const curve = Curves.easeInOut;
 
-      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-      var offsetAnimation = animation.drive(tween);
+        var tween =
+            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+        var offsetAnimation = animation.drive(tween);
 
-      return pushInto
-          ? FadeThroughTransition(
-              animation: animation,
-              secondaryAnimation: AlwaysStoppedAnimation(0.0),
-              child: child,
-            )
-          : SlideTransition(
-              position: offsetAnimation,
-              child: child,
-            );
-    },
-  );
+        return pushInto
+            ? FadeThroughTransition(
+                animation: animation,
+                secondaryAnimation: AlwaysStoppedAnimation(0.0),
+                child: child,
+              )
+            : SlideTransition(
+                position: offsetAnimation,
+                child: child,
+              );
+      },
+    );
+  }
 }
 
 void pushTo(BuildContext context, Widget widget, {bool pushInto = false}) {
