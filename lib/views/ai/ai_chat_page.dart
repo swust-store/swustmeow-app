5d189ad59e66626b2044c19d96fc7871c7b765db
FoliageOwO <mc.windleaf@foxmail.com>
2025-03-05 12:08:11 +0800

    fix(ai-chat): 删除模型选择器并优化页面样式

--- lib/views/ai/ai_chat_page.dart
+++ lib/views/ai/ai_chat_page.dart
@@ -29,7 +29,8 @@ class _AIChatPageState extends State<AIChatPage> with TickerProviderStateMixin {
   final List<AIChatMessage> _messages = [];
   late AIModel _selectedModel;
   bool _isLoading = false;
-  late FPopoverController _modelSelectController;
+
+  // late FPopoverController _modelSelectController;
   bool _isSearchEnabled = false;
   bool _showScrollToBottom = false;
 
@@ -37,19 +38,22 @@ class _AIChatPageState extends State<AIChatPage> with TickerProviderStateMixin {
   void initState() {
     super.initState();
     _selectedModel = AIChatTestData.models.first;
-    _modelSelectController = FPopoverController(vsync: this);
-    _messages.addAll(AIChatTestData.sampleMessages);
+    // _modelSelectController = FPopoverController(vsync: this);
+    // _messages.addAll(AIChatTestData.sampleMessages);
 
     WidgetsBinding.instance.addPostFrameCallback((_) {
       KeyboardVisibilityController().onChange.listen((bool visible) {
-        if (visible) {
-          _scrollToBottom();
+        if (visible && mounted) {
+          Future.delayed(const Duration(milliseconds: 200), () {
+            _scrollToBottom(jumpTo: true);
+          });
         }
       });
     });
 
     _scrollController.addListener(() {
-      final showButton = _scrollController.position.pixels > 0;
+      final showButton = _scrollController.position.pixels <
+          _scrollController.position.maxScrollExtent;
       if (showButton != _showScrollToBottom) {
         setState(() {
           _showScrollToBottom = showButton;
@@ -62,27 +66,23 @@ class _AIChatPageState extends State<AIChatPage> with TickerProviderStateMixin {
   void dispose() {
     _messageController.dispose();
     _scrollController.dispose();
-    _modelSelectController.dispose();
+    // _modelSelectController.dispose();
     super.dispose();
   }
 
-  void _refresh([Function()? fn]) {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      if (!mounted) return;
-      setState(fn ?? () {});
-    });
-  }
-
-  void _scrollToBottom() {
+  void _scrollToBottom({bool jumpTo = false}) {
     WidgetsBinding.instance.addPostFrameCallback((_) {
       if (!mounted) return;
       if (_scrollController.hasClients) {
-        _scrollController.animateTo(
-          0,
+        jumpTo ? _scrollController.jumpTo(
+          _scrollController.position.maxScrollExtent,
+        ) : _scrollController.animateTo(
+          _scrollController.position.maxScrollExtent,
           duration: const Duration(milliseconds: 300),
           curve: Curves.easeOut,
         );
       }
+      // setState(() => _showScrollToBottom = false);
     });
   }
 
@@ -104,12 +104,12 @@ class _AIChatPageState extends State<AIChatPage> with TickerProviderStateMixin {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            suffixIcons: [
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8.0),
-                child: _buildModelSelector(),
-              ),
-            ],
+            // suffixIcons: [
+            //   Padding(
+            //     padding: EdgeInsets.symmetric(horizontal: 8.0),
+            //     child: _buildModelSelector(),
+            //   ),
+            // ],
           ),
           content: Column(
             children: [
@@ -124,151 +124,172 @@ class _AIChatPageState extends State<AIChatPage> with TickerProviderStateMixin {
     );
   }
 
-  Widget _buildModelSelector() {
-    return FPopover(
-      controller: _modelSelectController,
-      childAnchor: Alignment.topCenter,
-      popoverAnchor: Alignment.bottomCenter,
-      child: FTappable(
-        onPress: () {
-          _modelSelectController.toggle();
-        },
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-          decoration: BoxDecoration(
-            color: Colors.white.withValues(alpha: 0.1),
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              if (_selectedModel.icon != null)
-                Padding(
-                  padding: EdgeInsets.only(right: 6),
-                  child: FaIcon(
-                    _selectedModel.icon,
-                    color: Colors.white,
-                    size: 14,
-                  ),
-                ),
-              Text(
-                _selectedModel.name,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 14,
-                ),
-              ),
-              const SizedBox(width: 2),
-              FIcon(
-                FAssets.icons.chevronsUpDown,
-                color: Colors.white,
-                size: 14,
-              )
-            ],
-          ),
-        ),
-      ),
-      popoverBuilder: (context, style, child) => Container(
-        width: 240,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(8),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withValues(alpha: 0.1),
-              blurRadius: 10,
-              offset: Offset(0, 4),
-            ),
-          ],
-        ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: AIChatTestData.models
-              .map((model) => _buildModelItem(model))
-              .toList(),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildModelItem(AIModel model) {
-    final isSelected = model.id == _selectedModel.id;
-    return InkWell(
-      onTap: () {
-        _refresh(() => _selectedModel = model);
-        _modelSelectController.hide();
-      },
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-        decoration: BoxDecoration(
-          color: isSelected
-              ? MTheme.primary2.withValues(alpha: 0.1)
-              : Colors.transparent,
-        ),
-        child: Row(
-          children: [
-            if (model.icon != null)
-              Padding(
-                padding: EdgeInsets.only(right: 12),
-                child: FaIcon(
-                  model.icon,
-                  color: isSelected ? MTheme.primary2 : Colors.grey,
-                  size: 16,
-                ),
-              ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    model.name,
-                    style: TextStyle(
-                      color: isSelected ? MTheme.primary2 : Colors.black,
-                      fontWeight:
-                          isSelected ? FontWeight.bold : FontWeight.normal,
-                    ),
-                  ),
-                  if (model.description != null)
-                    Text(
-                      model.description!,
-                      style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 12,
-                      ),
-                    ),
-                ],
-              ),
-            ),
-            if (isSelected)
-              FaIcon(
-                FontAwesomeIcons.check,
-                color: MTheme.primary2,
-                size: 14,
-              ),
-          ],
-        ),
-      ),
-    );
-  }
+  // Widget _buildModelSelector() {
+  //   return FPopover(
+  //     controller: _modelSelectController,
+  //     childAnchor: Alignment.topCenter,
+  //     popoverAnchor: Alignment.bottomCenter,
+  //     child: FTappable(
+  //       onPress: () {
+  //         _modelSelectController.toggle();
+  //       },
+  //       child: Container(
+  //         padding: EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+  //         decoration: BoxDecoration(
+  //           color: Colors.white.withValues(alpha: 0.1),
+  //           borderRadius: BorderRadius.circular(20),
+  //         ),
+  //         child: Row(
+  //           mainAxisSize: MainAxisSize.min,
+  //           children: [
+  //             if (_selectedModel.icon != null)
+  //               Padding(
+  //                 padding: EdgeInsets.only(right: 6),
+  //                 child: FaIcon(
+  //                   _selectedModel.icon,
+  //                   color: Colors.white,
+  //                   size: 14,
+  //                 ),
+  //               ),
+  //             Text(
+  //               _selectedModel.name,
+  //               style: TextStyle(
+  //                 color: Colors.white,
+  //                 fontSize: 14,
+  //               ),
+  //             ),
+  //             const SizedBox(width: 2),
+  //             FIcon(
+  //               FAssets.icons.chevronsUpDown,
+  //               color: Colors.white,
+  //               size: 14,
+  //             )
+  //           ],
+  //         ),
+  //       ),
+  //     ),
+  //     popoverBuilder: (context, style, child) => Container(
+  //       width: 240,
+  //       decoration: BoxDecoration(
+  //         color: Colors.white,
+  //         borderRadius: BorderRadius.circular(8),
+  //         boxShadow: [
+  //           BoxShadow(
+  //             color: Colors.black.withValues(alpha: 0.1),
+  //             blurRadius: 10,
+  //             offset: Offset(0, 4),
+  //           ),
+  //         ],
+  //       ),
+  //       child: Column(
+  //         mainAxisSize: MainAxisSize.min,
+  //         children: AIChatTestData.models
+  //             .map((model) => _buildModelItem(model))
+  //             .toList(),
+  //       ),
+  //     ),
+  //   );
+  // }
+  //
+  // Widget _buildModelItem(AIModel model) {
+  //   final isSelected = model.id == _selectedModel.id;
+  //   return InkWell(
+  //     onTap: () {
+  //       _refresh(() => _selectedModel = model);
+  //       _modelSelectController.hide();
+  //     },
+  //     child: Container(
+  //       padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+  //       decoration: BoxDecoration(
+  //         color: isSelected
+  //             ? MTheme.primary2.withValues(alpha: 0.1)
+  //             : Colors.transparent,
+  //       ),
+  //       child: Row(
+  //         children: [
+  //           if (model.icon != null)
+  //             Padding(
+  //               padding: EdgeInsets.only(right: 12),
+  //               child: FaIcon(
+  //                 model.icon,
+  //                 color: isSelected ? MTheme.primary2 : Colors.grey,
+  //                 size: 16,
+  //               ),
+  //             ),
+  //           Expanded(
+  //             child: Column(
+  //               crossAxisAlignment: CrossAxisAlignment.start,
+  //               children: [
+  //                 Text(
+  //                   model.name,
+  //                   style: TextStyle(
+  //                     color: isSelected ? MTheme.primary2 : Colors.black,
+  //                     fontWeight:
+  //                         isSelected ? FontWeight.bold : FontWeight.normal,
+  //                   ),
+  //                 ),
+  //                 if (model.description != null)
+  //                   Text(
+  //                     model.description!,
+  //                     style: TextStyle(
+  //                       color: Colors.grey,
+  //                       fontSize: 12,
+  //                     ),
+  //                   ),
+  //               ],
+  //             ),
+  //           ),
+  //           if (isSelected)
+  //             FaIcon(
+  //               FontAwesomeIcons.check,
+  //               color: MTheme.primary2,
+  //               size: 14,
+  //             ),
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
 
   Widget _buildMessageList() {
     return Stack(
       alignment: Alignment.bottomCenter,
       children: [
-        ListView.builder(
-          controller: _scrollController,
-          padding: EdgeInsets.only(left: 8, right: 0, top: 16, bottom: 16),
-          itemCount: _messages.length,
-          reverse: true,
-          addAutomaticKeepAlives: false,
-          addRepaintBoundaries: true,
-          itemBuilder: (context, index) {
-            final message = _messages[_messages.length - 1 - index];
-            return RepaintBoundary(
-              child: _buildMessageItem(message),
-            );
-          },
-        ),
+        if (_messages.isEmpty)
+          Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(
+                  FontAwesomeIcons.robot,
+                  size: 48,
+                  color: Colors.grey.withValues(alpha: 0.3),
+                ),
+                SizedBox(height: 16),
+                Text(
+                  '开始和 AI 助手对话吧',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
+          )
+        else
+          ListView.builder(
+            controller: _scrollController,
+            padding: EdgeInsets.only(left: 8, right: 0, top: 16, bottom: 16),
+            itemCount: _messages.length,
+            addAutomaticKeepAlives: false,
+            addRepaintBoundaries: true,
+            itemBuilder: (context, index) {
+              final message = _messages[index];
+              return RepaintBoundary(
+                child: _buildMessageItem(message),
+              );
+            },
+          ),
         if (_showScrollToBottom)
           Positioned(
             bottom: 16,
@@ -491,7 +512,7 @@ class _AIChatPageState extends State<AIChatPage> with TickerProviderStateMixin {
       _isLoading = true;
     });
 
-    _scrollToBottom();
+    _scrollToBottom(jumpTo: true); // 发送用户消息后滚动
 
     // 添加一个空的 AI 消息用于显示加载状态
     final aiMessage = AIChatMessage.assistant(
@@ -503,13 +524,14 @@ class _AIChatPageState extends State<AIChatPage> with TickerProviderStateMixin {
       _messages.add(aiMessage);
     });
 
+    _scrollToBottom(); // AI 开始回复时滚动
+
     String responseText = '';
 
     await SWUSTStoreApiService.streamChat(
       prompt: text,
       useSearch: _isSearchEnabled,
       onToken: (token) {
-        // 直接在每个 token 到达时更新状态
         if (mounted) {
           setState(() {
             responseText += token;
@@ -519,6 +541,7 @@ class _AIChatPageState extends State<AIChatPage> with TickerProviderStateMixin {
               isReceiving: true,
             );
           });
+          _scrollToBottom(); // 每次收到新token时滚动
         }
       },
       onError: (error) {
@@ -530,6 +553,7 @@ class _AIChatPageState extends State<AIChatPage> with TickerProviderStateMixin {
           );
           _isLoading = false;
         });
+        _scrollToBottom(); // 发生错误时滚动
       },
       onComplete: () {
         if (!mounted) return;
@@ -540,9 +564,8 @@ class _AIChatPageState extends State<AIChatPage> with TickerProviderStateMixin {
           );
           _isLoading = false;
         });
+        _scrollToBottom(); // 完成时滚动
       },
     );
-
-    _scrollToBottom();
   }
 }
